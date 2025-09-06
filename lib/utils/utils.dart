2a0338f9f143c56ca1d60927dbf08d9dfc7852ae
mabo <mabo@gmail.com>
2025-09-06 20:32:15 +0800

    update

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,24 +1,5 @@
-// ignore_for_file: prefer_function_declarations_over_variables, prefer_final_fields
-
-import 'dart:async';
-import 'dart:convert';
-import 'dart:io';
 import 'dart:math';
-import 'package:bot_toast/bot_toast.dart';
-import 'package:card_swiper/card_swiper.dart';
-import 'package:common_utils/common_utils.dart';
-import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/gestures.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:html_unescape/html_unescape.dart';
-import 'package:provider/provider.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:universal_html/html.dart' as html;
+
 
 class Utils {
   //随机字符串
